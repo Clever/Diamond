d280415b4fc3d3e7fc6390e8e4287750828e3041
Daniel Fuentes <daniel.fuentes@clever.com>
2016-07-06 14:48:45 -0700

    update tests for docker_stats collector

--- src/collectors/docker_stats/test/testdockerstats.py
+++ src/collectors/docker_stats/test/testdockerstats.py
@@ -72,7 +72,11 @@ def get_client_mock():
     u'Id': u'146979a5328952af505cd43123b45b06c38db8679aaadb2a4c18ad699a5cbeec',
     u'Name': u'test',
     u'Config': {
-      u'Env': ["TEST=/new/name/"]
+      u'Env': ["TEST=/new/name/"],
+      u'Labels': {
+        u'tag': u'ecs--name',
+        u'com.amazonaws.ecs.task-arn': u'arn:aws:ecs:us-west-1:000000000000:task/abcdef12-3456-789a-bcde-f123456789ab',
+      }
     }
   }
   return client_mock
@@ -97,24 +101,24 @@ class TestDockerStatsCollector(CollectorTestCase):
     docker_client_mock.return_value = client_mock
     self.collector.collect()
     metrics = {
-      'test.146979a53289.mem.rss': 100,
-      'test.146979a53289.mem.limit': 500,
-      'test.146979a53289.net.tx_bytes': 100,
-      'test.146979a53289.net.rx_bytes': 100,
+      'test.146979a53289.docker.mem.rss': 100,
+      'test.146979a53289.docker.mem.limit': 500,
+      'test.146979a53289.docker.net.tx_bytes': 100,
+      'test.146979a53289.docker.net.rx_bytes': 100,
     }
     self.assertPublishedMany(publish_mock, metrics)
 
     self.collector.collect()
     metrics = {
-      'test.146979a53289.mem.rss': 200,
-      'test.146979a53289.mem.limit': 500,
-      'test.146979a53289.net.tx_bytes': 200,
-      'test.146979a53289.net.rx_bytes': 200,
-      'test.146979a53289.cpu0.user': 1,
-      'test.146979a53289.cpu1.user': 2,
-      'test.146979a53289.cpu2.user': 3,
-      'test.146979a53289.cpu3.user': 4,
-      'test.146979a53289.cpu_total.user': 10
+      'test.146979a53289.docker.mem.rss': 200,
+      'test.146979a53289.docker.mem.limit': 500,
+      'test.146979a53289.docker.net.tx_bytes': 200,
+      'test.146979a53289.docker.net.rx_bytes': 200,
+      'test.146979a53289.docker.cpu0.user': 1,
+      'test.146979a53289.docker.cpu1.user': 2,
+      'test.146979a53289.docker.cpu2.user': 3,
+      'test.146979a53289.docker.cpu3.user': 4,
+      'test.146979a53289.docker.cpu_total.user': 10
     }
     self.assertPublishedMany(publish_mock, metrics)
 
@@ -138,24 +142,24 @@ class TestDockerStatsCollectorWithEnv(CollectorTestCase):
     docker_client_mock.return_value = client_mock
     self.collector.collect()
     metrics = {
-      'new.name.146979a53289.mem.rss': 100,
-      'new.name.146979a53289.mem.limit': 500,
-      'new.name.146979a53289.net.tx_bytes': 100,
-      'new.name.146979a53289.net.rx_bytes': 100,
+      'new.name.146979a53289.docker.mem.rss': 100,
+      'new.name.146979a53289.docker.mem.limit': 500,
+      'new.name.146979a53289.docker.net.tx_bytes': 100,
+      'new.name.146979a53289.docker.net.rx_bytes': 100,
     }
     self.assertPublishedMany(publish_mock, metrics)
 
     self.collector.collect()
     metrics = {
-      'new.name.146979a53289.mem.rss': 200,
-      'new.name.146979a53289.mem.limit': 500,
-      'new.name.146979a53289.net.tx_bytes': 200,
-      'new.name.146979a53289.net.rx_bytes': 200,
-      'new.name.146979a53289.cpu0.user': 1,
-      'new.name.146979a53289.cpu1.user': 2,
-      'new.name.146979a53289.cpu2.user': 3,
-      'new.name.146979a53289.cpu3.user': 4,
-      'new.name.146979a53289.cpu_total.user': 10
+      'new.name.146979a53289.docker.mem.rss': 200,
+      'new.name.146979a53289.docker.mem.limit': 500,
+      'new.name.146979a53289.docker.net.tx_bytes': 200,
+      'new.name.146979a53289.docker.net.rx_bytes': 200,
+      'new.name.146979a53289.docker.cpu0.user': 1,
+      'new.name.146979a53289.docker.cpu1.user': 2,
+      'new.name.146979a53289.docker.cpu2.user': 3,
+      'new.name.146979a53289.docker.cpu3.user': 4,
+      'new.name.146979a53289.docker.cpu_total.user': 10
     }
     self.assertPublishedMany(publish_mock, metrics)
 
@@ -179,23 +183,63 @@ class TestDockerStatsCollectorWithoutReplaceSlashes(CollectorTestCase):
     docker_client_mock.return_value = client_mock
     self.collector.collect()
     metrics = {
-      '/new/name/.146979a53289.mem.rss': 100,
-      '/new/name/.146979a53289.mem.limit': 500,
-      '/new/name/.146979a53289.net.tx_bytes': 100,
-      '/new/name/.146979a53289.net.rx_bytes': 100,
+      '/new/name/.146979a53289.docker.mem.rss': 100,
+      '/new/name/.146979a53289.docker.mem.limit': 500,
+      '/new/name/.146979a53289.docker.net.tx_bytes': 100,
+      '/new/name/.146979a53289.docker.net.rx_bytes': 100,
     }
     self.assertPublishedMany(publish_mock, metrics)
 
     self.collector.collect()
     metrics = {
-      '/new/name/.146979a53289.mem.rss': 200,
-      '/new/name/.146979a53289.mem.limit': 500,
-      '/new/name/.146979a53289.net.tx_bytes': 200,
-      '/new/name/.146979a53289.net.rx_bytes': 200,
-      '/new/name/.146979a53289.cpu0.user': 1,
-      '/new/name/.146979a53289.cpu1.user': 2,
-      '/new/name/.146979a53289.cpu2.user': 3,
-      '/new/name/.146979a53289.cpu3.user': 4,
-      '/new/name/.146979a53289.cpu_total.user': 10
+      '/new/name/.146979a53289.docker.mem.rss': 200,
+      '/new/name/.146979a53289.docker.mem.limit': 500,
+      '/new/name/.146979a53289.docker.net.tx_bytes': 200,
+      '/new/name/.146979a53289.docker.net.rx_bytes': 200,
+      '/new/name/.146979a53289.docker.cpu0.user': 1,
+      '/new/name/.146979a53289.docker.cpu1.user': 2,
+      '/new/name/.146979a53289.docker.cpu2.user': 3,
+      '/new/name/.146979a53289.docker.cpu3.user': 4,
+      '/new/name/.146979a53289.docker.cpu_total.user': 10
+    }
+    self.assertPublishedMany(publish_mock, metrics)
+
+class TestDockerStatsCollectorWithECSMode(CollectorTestCase):
+  def setUp(self):
+    config = get_collector_config('DockerStatsCollector', {
+      'client_url': 'localhost:4243',
+      'interval': 1,
+      'ecs_mode': True,
+    })
+    self.collector = DockerStatsCollector(config, None)
+
+  def test_import(self):
+    self.assertTrue(DockerStatsCollector)
+
+  @patch.object(Collector, 'publish')
+  @patch('docker.Client')
+  def test_should_publish_values_correctly(self, docker_client_mock, publish_mock):
+    client_mock = get_client_mock()
+    docker_client_mock.return_value = client_mock
+    self.collector.collect()
+    metrics = {
+      'ecs.name.abcdef12.docker.mem.rss': 100,
+      'ecs.name.abcdef12.docker.mem.limit': 500,
+      'ecs.name.abcdef12.docker.net.tx_bytes': 100,
+      'ecs.name.abcdef12.docker.net.rx_bytes': 100,
+    }
+    self.assertPublishedMany(publish_mock, metrics)
+
+    self.collector.collect()
+    metrics = {
+      'ecs.name.abcdef12.docker.mem.rss': 200,
+      'ecs.name.abcdef12.docker.mem.limit': 500,
+      'ecs.name.abcdef12.docker.net.tx_bytes': 200,
+      'ecs.name.abcdef12.docker.net.rx_bytes': 200,
+      'ecs.name.abcdef12.docker.cpu0.user': 1,
+      'ecs.name.abcdef12.docker.cpu1.user': 2,
+      'ecs.name.abcdef12.docker.cpu2.user': 3,
+      'ecs.name.abcdef12.docker.cpu3.user': 4,
+      'ecs.name.abcdef12.docker.cpu_total.user': 10
     }
     self.assertPublishedMany(publish_mock, metrics)
